16343c04e67ff8bde5b675f511a0a1641597dd3c
Takeshi Morita <takesi.morita@gmail.com>
2015-03-05 01:32:46 +0900

    Supporting JWO
    * Supporting Japanese Wikipedia Ontology

--- src/main/java/jp/ac/keio/ae/comp/yamaguti/doddle/data/DODDLEDic.java
+++ src/main/java/jp/ac/keio/ae/comp/yamaguti/doddle/data/DODDLEDic.java
@@ -35,57 +35,68 @@ import jp.ac.keio.ae.comp.yamaguti.doddle.utils.*;
  */
 public class DODDLEDic {
 
-    private static boolean isEDREnable() {
-        return DODDLE.getCurrentProject().getOntologySelectionPanel().isEDREnable();
-    }
+	private static boolean isEDREnable() {
+		return DODDLE.getCurrentProject().getOntologySelectionPanel().isEDREnable();
+	}
 
-    private static boolean isEDRTEnable() {
-        return DODDLE.getCurrentProject().getOntologySelectionPanel().isEDRTEnable();
-    }
+	private static boolean isEDRTEnable() {
+		return DODDLE.getCurrentProject().getOntologySelectionPanel().isEDRTEnable();
+	}
 
-    private static boolean isWordNetEnable() {
-        return DODDLE.getCurrentProject().getOntologySelectionPanel().isWordNetEnable();
-    }
-    
-    private static boolean isJPNWordNetEnable() {
-        return DODDLE.getCurrentProject().getOntologySelectionPanel().isJpnWordNetEnable();
-    }
+	private static boolean isWordNetEnable() {
+		return DODDLE.getCurrentProject().getOntologySelectionPanel().isWordNetEnable();
+	}
 
-    public static Concept getConcept(String uri) {
-        Resource res = ResourceFactory.createResource(uri);
-        String id = Utils.getLocalName(res);
-        String ns = Utils.getNameSpace(res);
-        Concept c = OWLOntologyManager.getConcept(uri);
-        if (c != null) { return c; }
-        if (isEDREnable() && ns.equals(DODDLEConstants.EDR_URI)) {
-            return EDRDic.getEDRConcept(id);
-        } else if (isEDRTEnable() && ns.equals(DODDLEConstants.EDRT_URI)) {
-            return EDRDic.getEDRTConcept(id);
-        } else if (isWordNetEnable() && ns.equals(DODDLEConstants.WN_URI)) {
-            return WordNetDic.getWNConcept(id); 
-        } else if (isJPNWordNetEnable() && ns.equals(DODDLEConstants.JPN_WN_URI)) {
-            return JpnWordNetDic.getConcept(id); 
-        }
-        return null;
-    }
+	private static boolean isJPNWordNetEnable() {
+		return DODDLE.getCurrentProject().getOntologySelectionPanel().isJpnWordNetEnable();
+	}
 
-    public static Set<String> getURISet(String word) {
-        Set<String> uriSet = new HashSet<String>();
-        if (isEDREnable()) {
-            Set<String> idSet = EDRDic.getEDRIDSet(word);
-            for (String id : idSet) {
-                uriSet.add(DODDLEConstants.EDR_URI + id);
-            }
-        }
-        if (isEDRTEnable()) {
-            Set<String> idSet = EDRDic.getEDRTIDSet(word);
-            for (String id : idSet) {
-                uriSet.add(DODDLEConstants.EDRT_URI + id);
-            }
-        }
-        uriSet.addAll(WordNetDic.getURISet(word));
-        uriSet.addAll(JpnWordNetDic.getSynsetSet(word));
-        uriSet.addAll(OWLOntologyManager.getURISet(word));
-        return uriSet;
-    }
+	private static boolean isJWOEnable() {
+		return DODDLE.getCurrentProject().getOntologySelectionPanel().isJWOEnable();
+	}
+
+	public static Concept getConcept(String uri) {
+		Resource res = ResourceFactory.createResource(uri);
+		String id = Utils.getLocalName(res);
+		String ns = Utils.getNameSpace(res);
+		Concept c = OWLOntologyManager.getConcept(uri);
+		System.out.println(isJWOEnable() + ": " + ns);
+		if (!isJWOEnable() && ns.equals(DODDLEConstants.JWO_URI)) {
+			return null;
+		}
+		System.out.println(c);
+		if (c != null) {
+			return c;
+		}
+		if (isEDREnable() && ns.equals(DODDLEConstants.EDR_URI)) {
+			return EDRDic.getEDRConcept(id);
+		} else if (isEDRTEnable() && ns.equals(DODDLEConstants.EDRT_URI)) {
+			return EDRDic.getEDRTConcept(id);
+		} else if (isWordNetEnable() && ns.equals(DODDLEConstants.WN_URI)) {
+			return WordNetDic.getWNConcept(id);
+		} else if (isJPNWordNetEnable() && ns.equals(DODDLEConstants.JPN_WN_URI)) {
+			return JpnWordNetDic.getConcept(id);
+		}
+		return null;
+	}
+
+	public static Set<String> getURISet(String word) {
+		Set<String> uriSet = new HashSet<String>();
+		if (isEDREnable()) {
+			Set<String> idSet = EDRDic.getEDRIDSet(word);
+			for (String id : idSet) {
+				uriSet.add(DODDLEConstants.EDR_URI + id);
+			}
+		}
+		if (isEDRTEnable()) {
+			Set<String> idSet = EDRDic.getEDRTIDSet(word);
+			for (String id : idSet) {
+				uriSet.add(DODDLEConstants.EDRT_URI + id);
+			}
+		}
+		uriSet.addAll(WordNetDic.getURISet(word));
+		uriSet.addAll(JpnWordNetDic.getSynsetSet(word));
+		uriSet.addAll(OWLOntologyManager.getURISet(word));
+		return uriSet;
+	}
 }
--- src/main/java/jp/ac/keio/ae/comp/yamaguti/doddle/utils/OWLOntologyManager.java
+++ src/main/java/jp/ac/keio/ae/comp/yamaguti/doddle/utils/OWLOntologyManager.java
@@ -26,6 +26,7 @@ package jp.ac.keio.ae.comp.yamaguti.doddle.utils;
 import java.io.*;
 import java.util.*;
 
+import jp.ac.keio.ae.comp.yamaguti.doddle.DODDLE;
 import jp.ac.keio.ae.comp.yamaguti.doddle.data.*;
 import jp.ac.keio.ae.comp.yamaguti.doddle.ui.*;
 
@@ -36,154 +37,174 @@ import com.hp.hpl.jena.rdf.model.*;
  */
 public class OWLOntologyManager {
 
-    private static Map<String, ReferenceOWLOntology> refOntMap = new HashMap<String, ReferenceOWLOntology>();
+	private static Map<String, ReferenceOWLOntology> refOntMap = new HashMap<String, ReferenceOWLOntology>();
 
-    public static void addRefOntology(String uri, ReferenceOWLOntology ontInfo) {
-        refOntMap.put(uri, ontInfo);
-    }
+	public static void addRefOntology(String uri, ReferenceOWLOntology ontInfo) {
+		refOntMap.put(uri, ontInfo);
+	}
 
-    public static void addRefOntology(File ontFile) {
-        try {
-            String uri = ontFile.getAbsolutePath();
-            Model ontModel = Utils
-                    .getOntModel(new FileInputStream(ontFile), "---", "RDF/XML", DODDLEConstants.BASE_URI);
-            ReferenceOWLOntology refOnt = new ReferenceOWLOntology(ontModel, uri, new NameSpaceTable());
-            addRefOntology(uri, refOnt);
-        } catch (FileNotFoundException fnfe) {
-            fnfe.printStackTrace();
-        }
-    }
+	public static void addRefOntology(File ontFile) {
+		try {
+			String uri = ontFile.getAbsolutePath();
+			Model ontModel = Utils.getOntModel(new FileInputStream(ontFile), "---", "RDF/XML",
+					DODDLEConstants.BASE_URI);
+			ReferenceOWLOntology refOnt = new ReferenceOWLOntology(ontModel, uri,
+					new NameSpaceTable());
+			addRefOntology(uri, refOnt);
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		}
+	}
 
-    public static ReferenceOWLOntology getRefOntology(String uri) {
-        return refOntMap.get(uri);
-    }
+	public static ReferenceOWLOntology getRefOntology(String uri) {
+		return refOntMap.get(uri);
+	}
 
-    public static void removeRefOntology(String uri) {
-        refOntMap.remove(uri);
-    }
+	public static void removeRefOntology(String uri) {
+		refOntMap.remove(uri);
+	}
 
-    public static Collection<ReferenceOWLOntology> getRefOntologySet() {
-        return refOntMap.values();
-    }
+	public static Collection<ReferenceOWLOntology> getRefOntologySet() {
+		return refOntMap.values();
+	}
 
-    public static Set<List<Concept>> getPathToRootSet(String uri) {
-        Set<List<Concept>> pathToRootSet = new HashSet<List<Concept>>();
-        for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
-            if (refOnt.isAvailable()) {
-                Set<List<Concept>> set = refOnt.getPathToRootSet(uri);
-                if (set != null) {
-                    pathToRootSet.addAll(set);
-                }
-            }
-        }
-        return pathToRootSet;
-    }
+	public static Set<List<Concept>> getPathToRootSet(String uri) {
+		Set<List<Concept>> pathToRootSet = new HashSet<List<Concept>>();
+		for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
+			if (refOnt.isAvailable()) {
+				Set<List<Concept>> set = refOnt.getPathToRootSet(uri);
+				if (set != null) {
+					pathToRootSet.addAll(set);
+				}
+			}
+		}
+		return pathToRootSet;
+	}
 
-    public static Set<List<String>> getURIPathToRootSet(String uri) {
-        Set<List<String>> pathToRootSet = new HashSet<List<String>>();
-        for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
-            if (refOnt.isAvailable()) {
-                Set<List<String>> set = refOnt.getURIPathToRootSet(uri);
-                if (set != null) {
-                    pathToRootSet.addAll(set);
-                }
-            }
-        }
-        return pathToRootSet;
-    }
+	public static Set<List<String>> getURIPathToRootSet(String uri) {
+		Set<List<String>> pathToRootSet = new HashSet<List<String>>();
+		for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
+			if (refOnt.isAvailable()) {
+				Set<List<String>> set = refOnt.getURIPathToRootSet(uri);
+				if (set != null) {
+					pathToRootSet.addAll(set);
+				}
+			}
+		}
+		return pathToRootSet;
+	}
 
-    public static Set<Concept> getVerbConceptSet(Set<Concept> inputConceptSet) {
-        Set<Concept> verbConceptSet = new HashSet<Concept>();
-        for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
-            if (refOnt.isAvailable()) {
-                for (String uri : refOnt.getPropertySet()) {
-                    Concept c = refOnt.getConcept(uri);
-                    if (inputConceptSet.contains(c)) {
-                        verbConceptSet.add(c);
-                    }
-                }
-            }
-        }
-        return verbConceptSet;
-    }
+	public static Set<Concept> getVerbConceptSet(Set<Concept> inputConceptSet) {
+		Set<Concept> verbConceptSet = new HashSet<Concept>();
+		for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
+			if (refOnt.isAvailable()) {
+				for (String uri : refOnt.getPropertySet()) {
+					Concept c = refOnt.getConcept(uri);
+					if (inputConceptSet.contains(c)) {
+						verbConceptSet.add(c);
+					}
+				}
+			}
+		}
+		return verbConceptSet;
+	}
 
-    public static Concept getConcept(String uri) {
-        Concept concept = null;
-        for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
-            if (refOnt.isAvailable()) {
-                concept = refOnt.getConcept(uri);
-            }
-        }
-        return concept;
-    }
+	public static Concept getConcept(String uri) {
+		Concept concept = null;
+		for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
+			if (refOnt.isAvailable()) {
+				concept = refOnt.getConcept(uri);
+			}
+		}
+		return concept;
+	}
 
-    public static Set<String> getDomainSet(Concept c, List<List<Concept>> trimmedConceptList) {
-        Set<String> domainSet = new HashSet<String>();
-        for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
-            if (refOnt.isAvailable()) {
-                domainSet.addAll(refOnt.getDomainSet(c.getURI()));
-                for (List<Concept> list : trimmedConceptList) {
-                    for (Concept trimmedConcept : list) {
-                        domainSet.addAll(refOnt.getDomainSet(trimmedConcept.getURI()));
-                    }
-                }
-            }
-        }
-        return domainSet;
-    }
+	public static Set<String> getDomainSet(Concept c, List<List<Concept>> trimmedConceptList) {
+		Set<String> domainSet = new HashSet<String>();
+		for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
+			if (refOnt.isAvailable()) {
+				domainSet.addAll(refOnt.getDomainSet(c.getURI()));
+				for (List<Concept> list : trimmedConceptList) {
+					for (Concept trimmedConcept : list) {
+						domainSet.addAll(refOnt.getDomainSet(trimmedConcept.getURI()));
+					}
+				}
+			}
+		}
+		return domainSet;
+	}
 
-    public static Set<String> getRangeSet(Concept c, List<List<Concept>> trimmedConceptList) {
-        Set<String> rangeSet = new HashSet<String>();
-        for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
-            if (refOnt.isAvailable()) {
-                rangeSet.addAll(refOnt.getRangeSet(c.getURI()));
-                for (List<Concept> list : trimmedConceptList) {
-                    for (Concept trimmedConcept : list) {
-                        rangeSet.addAll(refOnt.getRangeSet(trimmedConcept.getURI()));
-                    }
-                }
-            }
-        }
-        return rangeSet;
-    }
+	public static Set<String> getRangeSet(Concept c, List<List<Concept>> trimmedConceptList) {
+		Set<String> rangeSet = new HashSet<String>();
+		for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
+			if (refOnt.isAvailable()) {
+				rangeSet.addAll(refOnt.getRangeSet(c.getURI()));
+				for (List<Concept> list : trimmedConceptList) {
+					for (Concept trimmedConcept : list) {
+						rangeSet.addAll(refOnt.getRangeSet(trimmedConcept.getURI()));
+					}
+				}
+			}
+		}
+		return rangeSet;
+	}
 
-    public static Set<String> getSubURISet(String uri, Set<String> nounURISet) {
-        Set<String> subURISet = new HashSet<String>();
-        for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
-            if (refOnt.isAvailable()) {
-                Set<String> uriSet = new HashSet<String>();
-                refOnt.getSubURISet(uri, nounURISet, uriSet);
-                subURISet.addAll(uriSet);
-            }
-        }
-        return subURISet;
-    }
+	public static Set<String> getSubURISet(String uri, Set<String> nounURISet) {
+		Set<String> subURISet = new HashSet<String>();
+		for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
+			if (refOnt.isAvailable()) {
+				Set<String> uriSet = new HashSet<String>();
+				refOnt.getSubURISet(uri, nounURISet, uriSet);
+				subURISet.addAll(uriSet);
+			}
+		}
+		return subURISet;
+	}
 
-    public static Set<String> getURISet(String word) {
-        Set<String> uriSet = new HashSet<String>();
-        for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
-            if (refOnt.isAvailable()) {
-                Set<String> set = refOnt.getURISet(word);
-                if (set == null) {
-                    continue;
-                }
-                uriSet.addAll(set);
-            }
-        }
-        return uriSet;
-    }
+	public static Set<String> getURISet(String word) {
+		Set<String> uriSet = new HashSet<String>();
+		for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
+			if (refOnt.isAvailable()) {
+				Set<String> set = refOnt.getURISet(word);
+				if (set == null) {
+					continue;
+				}
+				uriSet.addAll(set);
+			}
+		}
+		System.out.println("jwo: "
+				+ DODDLE.getCurrentProject().getOntologySelectionPanel().isJWOEnable());
+		if (DODDLE.getCurrentProject().getOntologySelectionPanel().isJWOEnable()) {
+			return uriSet;
+		}
+		Set<String> newURISet = new HashSet<String>();
+		for (String uri : uriSet) {
+			if (!uri.contains(DODDLEConstants.JWO_URI)) {
+				System.out.println("added: " + uri);
+				newURISet.add(uri);
+			} else {
+				System.out.println("removed: " + uri);
+			}
+		}
+		return newURISet;
+	}
 
-    public static void setOWLConceptSet(String word, Set<Concept> conceptSet) {
-        for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
-            if (refOnt.isAvailable()) {
-                Set<String> uriSet = refOnt.getURISet(word);
-                if (uriSet == null) {
-                    continue;
-                }
-                for (String uri : uriSet) {
-                    conceptSet.add(refOnt.getConcept(uri));
-                }
-            }
-        }
-    }
+	public static void setOWLConceptSet(String word, Set<Concept> conceptSet) {
+		for (ReferenceOWLOntology refOnt : getRefOntologySet()) {
+			if (refOnt.isAvailable()) {
+				Set<String> uriSet = refOnt.getURISet(word);
+				if (uriSet == null) {
+					continue;
+				}
+				for (String uri : uriSet) {
+					if (!DODDLE.getCurrentProject().getOntologySelectionPanel().isJWOEnable()
+							&& uri.contains(DODDLEConstants.JWO_URI)) {
+						System.out.println("removed: " + uri);
+					} else {
+						conceptSet.add(refOnt.getConcept(uri));
+					}
+				}
+			}
+		}
+	}
 }
